65c50c679dddd60399d01ff00de483ae7ddadc74
Mathieu Drapeau <m.drapeau9@gmail.com>
2018-09-09 02:54:43 +0000

    fix update-registry.py

--- registry/update-registry.py
+++ registry/update-registry.py
@@ -12,7 +12,6 @@ docker_dir = "docker-images"
 def does_images_exist_on_registry(image_name):
     logging.info("Check if exist " + image_name)
 
-    p1 = subprocess.Popen(["git", "tag"], stdout=subprocess.PIPE)
     p1 = subprocess.Popen(["curl", "--insecure", "-X", "GET", "https://localhost:443/v2/_catalog"], stdout=subprocess.PIPE)
     json_str = str(p1.communicate()[0]).strip("b'\\n")
     json_obj = json.loads(json_str)
@@ -25,7 +24,7 @@ def does_images_exist_on_registry(image_name):
 
 def clean_up_registry():
 
-    logging.info("Clean up registry")
+    logging.info("Cleaning up registry")
 
     p1 = subprocess.Popen(["curl", "--insecure", "-X", "GET", "https://localhost:443/v2/_catalog"], stdout=subprocess.PIPE)
     json_str = str(p1.communicate()[0]).strip("b'\\n")
@@ -33,6 +32,7 @@ def clean_up_registry():
     repository_images = json_obj["repositories"]
     for i in range(0,len(repository_images)):
         if git_tag not in repository_images[i]:
+            logging.info("Delete image on registry : " + repository_images[i])
             shutil.rmtree("/mnt/registry/docker/registry/v2/repositories/" + repository_images[i])
 
 def clone_git_and_update_images():
@@ -50,6 +50,7 @@ def clone_git_and_update_images():
     logging.info("git tag")
     p1 = subprocess.Popen(["git", "tag"], stdout=subprocess.PIPE)
     p2 = subprocess.Popen(["tail", "-1"], stdin=p1.stdout, stdout=subprocess.PIPE)
+    global git_tag
     git_tag = str(p2.communicate()[0]).strip("b'\\n")
 
     subprocess.run(["git", "checkout", "tags/" + git_tag, "-b", "latest_tag"])
@@ -91,6 +92,7 @@ registry_url = sys.argv[2]
 
 while 1 :
     clone_git_and_update_images()
+    logging.info("Sleeping 2 min")
     time.sleep(120) # sleep 2 min
 
 
