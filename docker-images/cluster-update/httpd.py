f09ecfadc8fdc37ec3cfe8db535cb23932c06b69
Mathieu Drapeau <m.drapeau9@gmail.com>
2018-09-10 14:31:29 -0400

    Added cluster update to http server with kubctl

--- docker-images/cluster-update/httpd.py
+++ docker-images/cluster-update/httpd.py
@@ -6,6 +6,58 @@ Usage::
 """
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
+import os
+import json
+import subprocess
+
+secret_data = {}
+
+def read_secrets_config():
+    global secret_data
+    secret_data["images"] = {}
+
+    registry_url_path = "/mnt/registry/url"
+    if not os.path.isfile(registry_url_path):
+        throw("/mnt/registry/url doesn't exist")
+
+    registry_file = open(registry_url_path, "r")
+    secret_data["registry_url"] = registry_file.read()
+
+    images_root_path = "/mnt/images"
+    image_names = [d for d in os.listdir(images_root_path) if os.path.isdir(os.path.join(images_root_path, d))]
+
+    for image_name in image_names:
+        deployment_filepath = os.path.join(images_root_path,image_name,"deployment")
+        container_filepath = os.path.join(images_root_path,image_name,"container")
+        if os.path.isfile(deployment_filepath) and os.path.isfile(container_filepath):
+            f1 = open(deployment_filepath, "r")
+            deployment_name = f1.read()
+            f2 = open(container_filepath, "r")
+            container_name = f2.read()
+
+            secret_data["images"][image_name] = {}
+            secret_data["images"][image_name]["deployment"] = deployment_name
+            secret_data["images"][image_name]["container"] = container_name
+
+def check_and_update_deployment_image(decoded_post_data):
+    obj = json.loads(decoded_post_data)
+    event_obj = obj["events"][0]
+    if event_obj["action"] == "push":
+        if event_obj["target"]["mediaType"] == "application/vnd.docker.distribution.manifest.v2+json":
+            logging.info("Received new image notification")
+            new_image_name = event_obj["target"]["repository"]
+            base_image_name = new_image_name.split("_")[0]
+
+            # Deployment infos
+            deployment_name = secret_data["images"][base_image_name]["deployment"]
+            container_name = secret_data["images"][base_image_name]["container"]
+            registry_url = secret_data["registry_url"]
+            # Update cluster deployment image
+            run_command_arr = ["kubectl", "set", "image", "deployment/" + deployment_name, container_name + "=" + registry_url + "/" + new_image_name]
+            logging.info("Updating deployment : " + deployment_name + " to image " + new_image_name)
+            logging.info(run_command_arr)
+            subprocess.run(run_command_arr)
+
 
 class S(BaseHTTPRequestHandler):
     def _set_response(self):
@@ -21,8 +73,9 @@ class S(BaseHTTPRequestHandler):
     def do_POST(self):
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        decoded_post_data = post_data.decode('utf-8')
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
-                str(self.path), str(self.headers), post_data.decode('utf-8'))
+                str(self.path), str(self.headers), decoded_post_data)
 
         self._set_response()
         self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
@@ -42,6 +95,7 @@ def run(server_class=HTTPServer, handler_class=S, port=80):
 if __name__ == '__main__':
     from sys import argv
 
+    read_secrets_config()
     if len(argv) == 2:
         run(port=int(argv[1]))
     else:
